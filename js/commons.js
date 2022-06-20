8c539ceea18a170042bc0d9df37fda28ef8cfdfd
noemigp0 <64096227+noemigp0@users.noreply.github.com>
2022-06-20 15:59:14 -0500

    Update commons.js

--- js/commons.js
+++ js/commons.js
@@ -3,3 +3,17 @@ let idPost = window.location.search.substring(9);
 
 let url = "https://koder19g-ngp-default-rtdb.firebaseio.com/posts/.json"
 let urlUpd = `https://koder19g-ngp-default-rtdb.firebaseio.com/posts/${idPost}.json`
+
+//Esta funsion es para los alerts, el cdigo es de bootstrap
+  const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const alert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+};
